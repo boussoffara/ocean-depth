88b73a7e548e8ebde66881cab576f71cd63ce828
Alaa <boussoffara@me.com>
2019-03-13 20:09:54 +0100

    rnn

--- notebooks/bib.py
+++ notebooks/bib.py
@@ -4,12 +4,14 @@ from sklearn.metrics import mean_squared_error
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
-
-
+from scipy.special import erfinv
+from  tqdm import tqdm_notebook
 dataPath='../data/og/'
 lb = scipy.io.loadmat(dataPath +'VectLB19922008.mat')
 depths=lb['depth']
 del lb
+
+
 def loadData():
     lb = scipy.io.loadmat(dataPath +'VectLB19922008.mat')
     labels = [k[0] for k in lb['labels'][0] ]
@@ -35,6 +37,119 @@ def split(Xvars,Yvars):
         xvals.append(val.loc[val['year']==i,Xvars])
         yvals.append(val.loc[val['year']==i,Yvars])
     return xtrains,ytrains,xvals,yvals,xtest,ytest
+
+
+def gausSplit(Xvars,Yvars):
+    lb = scipy.io.loadmat(dataPath +'VectLB19922008.mat')
+    labels = [k[0] for k in lb['labels'][0] ]
+    data=pd.DataFrame(lb['Vect'],columns=labels)
+
+    i = np.argsort( data.loc[:,Xvars], axis = 0 )
+    j = np.argsort( i, axis = 0 )
+    j_range = len( j ) - 1
+    divider = j_range / (2-0.002)
+    transformed = j / divider
+    transformed = transformed - 1+0.001
+    transformed = erfinv( transformed )
+    data.loc[:,Xvars]=transformed
+
+
+    dataCenter=data[np.logical_and(np.logical_and(np.logical_and(data['latitude']<=33, data['latitude']>=31 ), data['longitude']>=-65 ), data['longitude']<=-63 )]
+    val=dataCenter[dataCenter['year']!=2008]
+    test=dataCenter[dataCenter['year']==2008]
+    train=data[data['year']!=2008]
+
+
+
+    xtrains=[]
+    ytrains=[]
+    xvals=[]
+    yvals=[]
+    xtest=test.loc[:,Xvars]
+    ytest=test.loc[:,Yvars]
+
+    for i in range(1992,2008):
+        xtrains.append(train.loc[train['year']!=i,Xvars])
+        ytrains.append(train.loc[train['year']!=i,Yvars])
+
+        xvals.append(val.loc[val['year']==i,Xvars])
+        yvals.append(val.loc[val['year']==i,Yvars])
+
+    return xtrains,ytrains,xvals,yvals,xtest,ytest
+
+def serialise(series,window,xvars,yvars,step=1):
+    X=[]
+    y=[]
+    tmin=series.loc[series.index[0],'time']
+    tcutMin=tmin
+    tcutMax=tmin+window
+
+    for k in range(1,len(series)-window+2,step):
+        X.append(series.loc[np.logical_and(series['time']>=tcutMin,series['time']<tcutMax),xvars].values)
+        y.append(series.loc[np.logical_and(series['time']>=tcutMin,series['time']<tcutMax),yvars].values)
+        tcutMin=tmin+(k%73)+(k//73)*100
+        tcutMax=tmin+((k+window)%73)+100*((k+window)//73)
+    X=np.array(X)
+    y=np.array(y)
+    return X,y
+
+def timeSplit(window,Xvars,Yvars,step=1,overlap=False):
+    lb = scipy.io.loadmat(dataPath +'VectLB19922008.mat')
+    labels = [k[0] for k in lb['labels'][0] ]
+    data=pd.DataFrame(lb['Vect'],columns=labels)
+    data['time']= data['year']*100+(data['5days']-1)
+    data=data.sort_values('time')
+    times=np.sort(list(set(data['time'])))
+    pos=set([(k,c)for k,c in zip(data['longitude'].values,data['latitude'].values) ])
+
+    center=[-64.02191162109375, 33.759124755859375]
+    selectCenter=np.logical_and(data['longitude'].values==center[0],
+                    data['latitude'].values==center[1])
+    test=data.loc[np.logical_and(data['year']==2008,selectCenter),:]
+    xtest,ytest=serialise(test,window,Xvars,Yvars,step)
+
+    if overlap==False :
+        xtrains=[]
+        ytrains=[]
+        xvals=[]
+        yvals=[]
+        for i in tqdm_notebook(range(1992,2008)):
+            val=data.loc[np.logical_and(data['year']==i,selectCenter),:]
+            a,b=serialise(val,window,Xvars,Yvars,step)
+            xvals.append(a)
+            yvals.append(b)
+
+            for i,p in enumerate(pos):
+                selectP=np.logical_and(data['longitude'].values==p[0],
+                data['latitude'].values==p[1])
+
+                tr1=data.loc[np.logical_and(np.logical_and(data['year']<2008,
+                                                           data['year']>i),selectP),:]
+                tr2=data.loc[np.logical_and(data['year']<i,selectP),:]
+
+                a1,b1=serialise(tr1,window,Xvars,Yvars,step)
+
+                if(i>1992):
+                    a2,b2=serialise(tr2,window,Xvars,Yvars,step)
+                    a=np.append(a1,a2,axis=0)
+                    b=np.append(b1,b2,axis=0)
+                else:
+                    a=a1
+                    b=b1
+
+
+                if(i==0):
+                    xi=a
+                    yi=b
+                else:
+                    xi=np.append(xi,a,axis=0)
+                    yi=np.append(yi,b,axis=0)
+
+            xtrains.append(np.array(xi))
+            ytrains.append(np.array(yi))
+
+    return xtrains,ytrains,xvals,yvals,xtest,ytest
+
 
 def applyToy(f,ytrains,yvals,ytest):
     return [f(y) for y in ytrains],[f(y) for y in yvals],f(ytest)
